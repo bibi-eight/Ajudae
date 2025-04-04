762400cdc25bca5e397a79d3bcbcede91e62d5ad
bibi-eight <bbritob8@gmail.com>
2025-04-04 10:55:29 -0300

    feat: inclusão area voluntariado e status enum

---
+++ src/Ajudae.Domain/Enums/AreaVoluntariadoEnum.cs
@@ -0,0 +1,12 @@
+namespace Ajudae.Domain.Enums;
+
+public enum AreaVoluntariadoEnum
+{
+    Tecnologia = 0,
+    ComunicacaoEhMarketing = 1,
+    CampanhasArrecadacao = 2,
+    EventosEhApoioGeral = 3,
+    HortaEhJardinagem = 4,
+    OficinasEducativas = 5,
+    Nutricao = 6
+}
---
+++ src/Ajudae.Domain/Enums/StatusEnum.cs
@@ -0,0 +1,10 @@
+namespace Ajudae.Domain.Enums;
+
+public enum StatusEnum
+{
+    Pendente = 0,
+    EmAndamento = 1,
+    Concluido = 2,
+    Cancelado = 3,
+    EmAtraso = 4
+}
