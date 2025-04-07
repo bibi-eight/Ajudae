a7bb227a27a9b8631139af3795315a367dabe4ab
bibi-eight <bbritob8@gmail.com>
2025-04-07 15:32:13 -0300

    feat: enriquecendo domínio de voluntários com métodos

--- src/Ajudae.Domain/Entities/Voluntario.cs
+++ src/Ajudae.Domain/Entities/Voluntario.cs
@@ -13,10 +13,13 @@ public class Voluntario : Entity, IAggregateRoot
     public int AtividadesFeitas { get; set; }
     public bool Presencial { get; set; }
     public bool Ativo { get; set; }
-    public List<Atividade> Atividades { get; set; }
-    public List<RecompensaItem> Recompensas { get; set; }
+    public ICollection<Atividade> atividades { get; set; }
+    public ICollection<RecompensaItem> recompensas { get; set; }
 
-    public Voluntario() { }
+    public Voluntario()
+    {
+        atividades = new List<Atividade>();
+    }
 
     public Voluntario(string nomeCompleto, string email, string telefone, AreaVoluntariadoEnum areaVoluntariado, bool presencial)
     {
@@ -25,6 +28,31 @@ public class Voluntario : Entity, IAggregateRoot
         Telefone = telefone;
         AreaVoluntariado = areaVoluntariado;
         Presencial = presencial;
+    }
+    
+    public void AtribuirNomeCompleto(string nomeCompleto) => NomeCompleto = nomeCompleto;
+    public void AtribuirEmail(string email) => Email = email;
+    public void AtribuirTelefone(string telefone) => Telefone = telefone;
+    public void AtribuirAreaVoluntariado(AreaVoluntariadoEnum areaVoluntariado) => AreaVoluntariado = areaVoluntariado;
+    public void AtribuirPontuacao(int pontos) => Pontuacao = pontos;
+    public void TornarPresencial(bool presencial) => Presencial = true;
+    public void TornarRemoto(bool presencial) => Presencial = false;
+    public void AtivarVoluntario() => Ativo = true;
+    public void DesativarVoluntario() => Ativo = false;
+
+    public void AdicionarAtividade(Atividade atividade)
+    {
+        atividades.Add(atividade);
+
+        if (atividade.Status == StatusEnum.Concluido)
+        {
+            AtividadesFeitas++;
+        }
+    }
+
+    public void AdicionarRecompensa(RecompensaItem recompensa)
+    {
+        recompensas.Add(recompensa);
     }
 }
 
