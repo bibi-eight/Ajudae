bdeeaa39265444debfc55ca93ea2c3b98504abf1
bibi-eight <bbritob8@gmail.com>
2025-04-10 15:36:31 -0300

    feat: interface de voluntarios

---
+++ src/Ajudae.Domain/Interfaces/IVoluntarioRepository.cs
@@ -0,0 +1,11 @@
+using Ajudae.Domain.Entities;
+using EstartandoDevsCore.Data;
+
+namespace Ajudae.Domain.Interfaces;
+
+public interface IVoluntarioRepository : IRepository<Voluntario>
+{
+    Task<IEnumerable<Voluntario>> ObterVoluntariosAtivos();
+    Task<IEnumerable<Voluntario>> ObterVoluntariosInativos();
+    Task<IEnumerable<Voluntario>> ObterVoluntarios();
+}
