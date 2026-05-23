c6b397683d90e13610abe1bc0ac49fe212e48e82
Brenda Brito <bbritob8@gmail.com>
2026-05-23 18:10:51 -0300

    feat: dependencias

--- src/Ajudae.App/Configuration/DependencyInjection.cs
+++ src/Ajudae.App/Configuration/DependencyInjection.cs
@@ -1,4 +1,10 @@
+using Ajudae.App.Application.Commands.Atividades;
+using Ajudae.App.Application.Commands.Voluntarios;
+using Ajudae.Domain.Interfaces;
+using Ajudae.Infra.Repositories;
 using EstartandoDevsCore.Mediator;
+using FluentValidation.Results;
+using MediatR;
 
 namespace Ajudae.App.Configuration;
 
@@ -8,5 +14,13 @@ public static class DependencyInjection
     {
         services.AddScoped<IMediatorHandler, MediatorHandler>();
 
+        services.AddScoped<IVoluntarioRepository, VoluntarioRepository>();
+        services.AddScoped<IAtividadeRepository, AtividadeRepository>();
+        
+        services.AddScoped<IRequestHandler<AdicionarAtividadeCommand, ValidationResult>, AtividadeCommandHandler>();
+        services.AddScoped<IRequestHandler<EditarAtividadeCommand, ValidationResult>, AtividadeCommandHandler>();
+        
+        services.AddScoped<IRequestHandler<AdicionarVoluntarioCommand, ValidationResult>, VoluntarioCommandHandler>();
+        services.AddScoped<IRequestHandler<EditarVoluntarioCommand, ValidationResult>, VoluntarioCommandHandler>();
     }
 }
