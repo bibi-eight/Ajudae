8e735460464b27e926b60922a5f3effb9c98b692
Brenda Brito <bbritob8@gmail.com>
2026-08-26 14:49:07 -0300

    .

--- src/Ajudae.App/obj/Debug/net8.0/Ajudae.App.AssemblyInfo.cs
+++ src/Ajudae.App/obj/Debug/net8.0/Ajudae.App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ajudae.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a2e5c8c7dec2693be8a56e3494d16a74d870690")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85f1b43b865b98ec07295b04d97476c7bc353dea")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ajudae.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ajudae.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/Ajudae.Domain/obj/Debug/net8.0/Ajudae.Domain.AssemblyInfo.cs
+++ src/Ajudae.Domain/obj/Debug/net8.0/Ajudae.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ajudae.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a2e5c8c7dec2693be8a56e3494d16a74d870690")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85f1b43b865b98ec07295b04d97476c7bc353dea")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ajudae.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ajudae.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
